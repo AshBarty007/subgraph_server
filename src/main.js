2e6ee9f0d5fdf09805773bf22ba95b9e7d544fac
learnforcode <2435264793@qq.com>
2022-04-14 10:45:21 +0800

    graphql ok

--- src/main.js
+++ src/main.js
@@ -10,9 +10,9 @@ var server = http.createServer((req, res) => {
 	res.end('shut down\n')
 })
 
-// let url = "mongodb://localhost:27017";
-// let MongoClient = mongodb.MongoClient;
-var RawV2SubgraphPool = {
+let url = "mongodb://localhost:27017";
+let MongoClient = mongodb.MongoClient;
+var pair = {
     id: "",
     token0: {
         id: "",
@@ -26,23 +26,26 @@ var RawV2SubgraphPool = {
     reserveETH: 0.0,
     trackedReserveETH: 0.0
 };
+var pairs = {pair,pair};
 
 graphql.query(graphql.QuickSwap, true, 100).then(res => {
-	RawV2SubgraphPool.id = res[0].id
+    for(var i=0;i<10;i++){
+        pairs[i] = res[i]
+    }
 }).catch(e => { console.log(e) });
 
-// MongoClient.connect(url, hostname, function (err, db) {
-// 	if (err) throw err;
-//     var dbo = db.db("BarterSwap");
-// 	var token0 = {id:"",symbol:""}
-// 	var token1 = {id:"",symbol:""}
-//     var pair = { id: "", token0: token0, token1: token1,totalSupply: 0, reserveETH:0,trackedReserveETH:0};
-//     dbo.collection("QuickSwap").insertOne(pair, function(err, res) {
-//         if (err) throw err;
-//         console.log("insert successfully");
-//         db.close();
-//     });
-// });
+MongoClient.connect(url, hostname, function (err, db) {
+	if (err) throw err;
+    var dbo = db.db("BarterSwap");
+	var token0 = {id:"",symbol:""}
+	var token1 = {id:"",symbol:""}
+    var pair = { id: "", token0: token0, token1: token1,totalSupply: 0, reserveETH:0,trackedReserveETH:0};
+    dbo.collection("QuickSwap").insertOne(pair, function(err, res) {
+        if (err) throw err;
+        console.log("insert successfully");
+        db.close();
+    });
+});
 
 server.listen(port, () => {
 	console.log("server is running...")
