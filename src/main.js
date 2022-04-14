2a580c8a543e30720f29d08ba69600ca4ac795a5
learnforcode <2435264793@qq.com>
2022-04-14 10:24:10 +0800

    var

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
-const http = require('http')
-const graphql = require('./graphql')
-const mongodb = require('mongodb')
+var http = require('http')
+var graphql = require('./graphql')
+var mongodb = require('mongodb')
 
-const hostname = "0.0.0.0"
-const port = 3000
-const server = http.createServer((req, res) => {
+var hostname = "0.0.0.0"
+var port = 3000
+var server = http.createServer((req, res) => {
 	res.statusCode = 200
 	res.setHeader('Content-Type', 'text/plain')
 	res.end('shut down\n')
@@ -12,16 +12,37 @@ const server = http.createServer((req, res) => {
 
 let url = "mongodb://localhost:27017";
 let MongoClient = mongodb.MongoClient;
+var RawV2SubgraphPool = {
+    id: string,
+    token0: {
+        id: string,
+        symbol: string,
+    },
+    token1: {
+        id: string,
+        symbol: string,
+    },
+    totalSupply: number,
+    reserveETH: number,
+    trackedReserveETH: number
+};
+
+graphql.query(graphql.QuickSwap, true, 100).then(res => {
+	RawV2SubgraphPool.id = res[0].id
+}).catch(e => { console.log(e) });
 
 MongoClient.connect(url, hostname, function (err, db) {
 	if (err) throw err;
-	console.log("Connected to MongoDB!");
-	db.close();
+    var dbo = db.db("BarterSwap");
+	var token0 = {id:"",symbol:""}
+	var token1 = {id:"",symbol:""}
+    var pair = { id: "", token0: token0, token1: token1,totalSupply: 0, reserveETH:0,trackedReserveETH:0};
+    dbo.collection("QuickSwap").insertOne(pair, function(err, res) {
+        if (err) throw err;
+        console.log("insert successfully");
+        db.close();
+    });
 });
-
-graphql.query(graphql.QuickSwap, true, 100).then(res => {
-	console.log(res)
-}).catch(e => { console.log(e) });
 
 server.listen(port, () => {
 	console.log("server is running...")
