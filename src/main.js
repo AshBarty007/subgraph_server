602564c4dbe2ffeebb06f78d081ff532d3b4faaa
learnforcode <2435264793@qq.com>
2022-04-15 10:54:19 +0800

    test server

--- src/main.js
+++ src/main.js
@@ -18,81 +18,81 @@ function UpdateData() {
         MongoClient.connect(url, function (err, db) {
             if (err) throw err;
             var dbo = db.db("BarterSwap");
-		/*
-            var whereStr = {};
-            dbo.collection("QuickSwap").deleteMany(whereStr, function (err, obj) {
-                if (err) throw err;
-                db.close();
-            });
-*/
+            /*
+                var whereStr = {};
+                dbo.collection("QuickSwap").deleteMany(whereStr, function (err, obj) {
+                    if (err) throw err;
+                    db.close();
+                });
+            */
             dbo.collection("QuickSwap").insertMany(pairs, function (err, res) {
                 if (err) throw err;
                 db.close();
             });
         });
     }).catch(e => { console.log(e) });
-/*
-    graphql.query(graphql.SushiSwap, true, 40).then(res => {
-        for (var i = 0; i < 40; i++) {
-            pairs[i] = res[i]
-        }
-        MongoClient.connect(url, function (err, db) {
-            if (err) throw err;
-            var dbo = db.db("BarterSwap");
-            var whereStr = {};
-            dbo.collection("SushiSwap").deleteMany(whereStr, function (err, obj) {
+    /*
+        graphql.query(graphql.SushiSwap, true, 40).then(res => {
+            for (var i = 0; i < 40; i++) {
+                pairs[i] = res[i]
+            }
+            MongoClient.connect(url, function (err, db) {
                 if (err) throw err;
-                db.close();
+                var dbo = db.db("BarterSwap");
+                var whereStr = {};
+                dbo.collection("SushiSwap").deleteMany(whereStr, function (err, obj) {
+                    if (err) throw err;
+                    db.close();
+                });
+            
+                dbo.collection("SushiSwap").insertMany(pairs, function (err, res) {
+                    if (err) throw err;
+                    db.close();
+                });
             });
-        
-            dbo.collection("SushiSwap").insertMany(pairs, function (err, res) {
+        }).catch(e => { console.log(e) });
+    
+    
+        graphql.query(graphql.SushiSwap, true, 100).then(res => {
+            for (var i = 0; i < 100; i++) {
+                pairs[i] = res[i]
+            }
+            MongoClient.connect(url, function (err, db) {
                 if (err) throw err;
-                db.close();
+                var dbo = db.db("BarterSwap");
+                var whereStr = {};
+                dbo.collection("ApeSwap").deleteMany(whereStr, function (err, obj) {
+                    if (err) throw err;
+                    db.close();
+                });
+            
+                dbo.collection("ApeSwap").insertMany(pairs, function (err, res) {
+                    if (err) throw err;
+                    db.close();
+                });
             });
-        });
-    }).catch(e => { console.log(e) });
-
-
-    graphql.query(graphql.SushiSwap, true, 100).then(res => {
-        for (var i = 0; i < 100; i++) {
-            pairs[i] = res[i]
-        }
-        MongoClient.connect(url, function (err, db) {
-            if (err) throw err;
-            var dbo = db.db("BarterSwap");
-            var whereStr = {};
-            dbo.collection("ApeSwap").deleteMany(whereStr, function (err, obj) {
+        }).catch(e => { console.log(e) });
+    
+        graphql.query(graphql.SushiSwap, true, 10).then(res => {
+            for (var i = 0; i < 10; i++) {
+                pairs[i] = res[i]
+            }
+            MongoClient.connect(url, function (err, db) {
                 if (err) throw err;
-                db.close();
+                var dbo = db.db("BarterSwap");
+                var whereStr = {};
+                dbo.collection("PancakeSwap").deleteMany(whereStr, function (err, obj) {
+                    if (err) throw err;
+                    db.close();
+                });
+            
+                dbo.collection("PancakeSwap").insertMany(pairs, function (err, res) {
+                    if (err) throw err;
+                    db.close();
+                });
             });
-        
-            dbo.collection("ApeSwap").insertMany(pairs, function (err, res) {
-                if (err) throw err;
-                db.close();
-            });
-        });
-    }).catch(e => { console.log(e) });
-
-    graphql.query(graphql.SushiSwap, true, 10).then(res => {
-        for (var i = 0; i < 10; i++) {
-            pairs[i] = res[i]
-        }
-        MongoClient.connect(url, function (err, db) {
-            if (err) throw err;
-            var dbo = db.db("BarterSwap");
-            var whereStr = {};
-            dbo.collection("PancakeSwap").deleteMany(whereStr, function (err, obj) {
-                if (err) throw err;
-                db.close();
-            });
-        
-            dbo.collection("PancakeSwap").insertMany(pairs, function (err, res) {
-                if (err) throw err;
-                db.close();
-            });
-        });
-    }).catch(e => { console.log(e) });
-    */
+        }).catch(e => { console.log(e) });
+        */
 }
 
 const scheduleTask = () => {
@@ -109,32 +109,26 @@ var server = http.createServer((req, res) => {
     const http_url = req.url;
     const http_path = http_url.split('/')[1];
     console.log('path is:', http_path);
-        if (http_path == ''){
-		        res.writeHead(200,{"Content-Type":"text/plain"});
-		        res.end("url error!");
-		    }else{
-			    console.log("return data")
-   
-    MongoClient.connect(url, function (err, db) {
-        if (err) throw err;
-        var dbo = db.db("BarterSwap");
-        dbo.collection("QuickSwap").find({}).toArray(function (err, result) {
+    if (http_path == '') {
+        res.writeHead(200, { "Content-Type": "text/plain" });
+        res.end("url error!");
+    } else {
+        MongoClient.connect(url, function (err, db) {
             if (err) throw err;
-            
-            
-            db.close();
-		if (result != null){
-			            res.writeHead(200, { "Content-Type": "application/json" });
-			            res.end(JSON.stringify(result));
-			        }else{
-					            res.writeHead(200,{"Content-Type":"text/plain"});
-					                        res.end("url error!");
-					        }
-        });
-	
-
-    })
-		    }
+            var dbo = db.db("BarterSwap");
+            dbo.collection("QuickSwap").find({}).toArray(function (err, result) {
+                if (err) throw err;
+                db.close();
+                if (result != null) {
+                    res.writeHead(200, { "Content-Type": "application/json" });
+                    res.end(JSON.stringify(result));
+                } else {
+                    res.writeHead(200, { "Content-Type": "text/plain" });
+                    res.end("url error!");
+                }
+            });
+        })
+    }
 });
 
 server.listen(port, hostname, () => {
--- src/mongodb.js
+++ src/mongodb.js
@@ -1,18 +1,40 @@
-var url = "mongodb://localhost:27017"; 
-var MongoClient = require('mongodb').MongoClient; 
+var url = "mongodb://localhost:27017";
+var MongoClient = require('mongodb').MongoClient;
 
-MongoClient.connect(url, function(err, db) { 
-    if (err) throw err; 
-    console.log("Connected to MongoDB!"); 
-    db.close(); 
+MongoClient.connect(url, function (err, db) {
+	if (err) throw err;
+	console.log("Connected to MongoDB!");
+	db.close();
 });
 
-    MongoClient.connect(url, function (err, db) {
-	            if (err) throw err;
-	            var dbo = db.db("BarterSwap");
-	            dbo.collection("QuickSwap").find({}).toArray(function (err, result) {
-			                if (err) throw err;
-			                console.log(result);
-			                db.close();
-			            });
-	        })
+MongoClient.connect(url, function (err, db) {
+	if (err) throw err;
+	var dbo = db.db("BarterSwap");
+
+	var whereStr = {};
+	dbo.collection("QuickSwap").deleteMany(whereStr, function (err, obj) {
+		if (err) throw err;
+		db.close();
+	});
+
+	dbo.collection("QuickSwap").find({}).toArray(function (err, result) {
+		if (err) throw err;
+		console.log(result);
+		db.close();
+	});
+})
+
+MongoClient.connect(url, function (err, db) {
+	if (err) throw err;
+	var dbo = db.db("BarterSwap");
+	var whereStr = {};
+	dbo.collection("SushiSwap").deleteMany(whereStr, function (err, obj) {
+		if (err) throw err;
+		db.close();
+	});
+
+	dbo.collection("SushiSwap").insertMany(pairs, function (err, res) {
+		if (err) throw err;
+		db.close();
+	});
+});
