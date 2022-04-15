81f458a58b33d9a5c0c807a7278587b574d54bcd
learnforcode <2435264793@qq.com>
2022-04-15 11:12:28 +0800

    test server

--- src/mongodb.js
+++ src/mongodb.js
@@ -7,6 +7,8 @@ MongoClient.connect(url, function (err, db) {
 	db.close();
 });
 
+var pairs = {"_id":"625806e74f8caecbc9bedc67","id":"0xedcb666e2279e02855cf3e28e8ccbfb9803e088f","token0":{"id":"0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270","symbol":"WMATIC"},"token1":{"id":"0xf3f3065910a65f7c7a7cbd04871b5bef27c53e56","symbol":"DISNEY"},"totalSupply":"1732.050807568877292527","reserveETH":"48706742942.30950104510364955938377","trackedReserveETH":"0.000000003848391839686172562452299042268264"}
+
 MongoClient.connect(url, function (err, db) {
 	if (err) throw err;
 	var dbo = db.db("BarterSwap");
@@ -14,12 +16,19 @@ MongoClient.connect(url, function (err, db) {
 	var whereStr = {};
 	dbo.collection("QuickSwap").deleteMany(whereStr, function (err, obj) {
 		if (err) throw err;
+		console.log("quick 1",obj);
+		db.close();
+	});
+
+	dbo.collection("QuickSwap").insertMany(pairs, function (err, res) {
+		if (err) throw err;
+		console.log("quick 2",res);
 		db.close();
 	});
 
 	dbo.collection("QuickSwap").find({}).toArray(function (err, result) {
 		if (err) throw err;
-		console.log(result);
+		console.log("quick 3",result);
 		db.close();
 	});
 })
@@ -30,11 +39,19 @@ MongoClient.connect(url, function (err, db) {
 	var whereStr = {};
 	dbo.collection("SushiSwap").deleteMany(whereStr, function (err, obj) {
 		if (err) throw err;
+		console.log("Sushi 1",obj);
 		db.close();
 	});
 
 	dbo.collection("SushiSwap").insertMany(pairs, function (err, res) {
 		if (err) throw err;
+		console.log("Sushi 2",res);
+		db.close();
+	});
+
+	dbo.collection("SushiSwap").find({}).toArray(function (err, result) {
+		if (err) throw err;
+		console.log("Sushi 3",result);
 		db.close();
 	});
 });
