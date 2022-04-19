4a6f3180f9f13f95b569647e25fb8a7474b86833
learnforcode <2435264793@qq.com>
2022-04-19 17:48:04 +0800

    test

--- test/test.js
+++ test/test.js
@@ -4,27 +4,38 @@ var mongodb = require('mongodb')
 const dburl = "mongodb://root:" + encodeURIComponent("Mr0s8#dFdf#8s386di2ds") + "@barterswap.cluster-ck74h9ydda33.ap-southeast-1.docdb.amazonaws.com:27017/?replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false";
 let MongoClient = mongodb.MongoClient;
 
-function UpdateData() {
-    graphql.query1(graphql.QuickSwap, 140).then(res => {
-		clearPairs();
-        updatePairs(res, "quickswap",137);
-    }).catch(e => { console.log(e) });
+async function UpdateData() {
+	var conn = null;
+    try {
+        conn = await MongoClient.connect(dburl);
+        let test = conn.db("BarterSwap").collection("Pairs");
+        // delete
+        test.deleteMany();
 
-    graphql.query1(graphql.SushiSwap, 40).then(res => {
-        updatePairs(res, "sushiswap",137);
-    }).catch(e => { console.log(e) });
-
-    graphql.query2(graphql.PancakeSwap, 10).then(res => {
-        updatePairs(res, "pancakeswap",56);
-    }).catch(e => { console.log(e) });
-
-    graphql.query3(graphql.UniSwap_v3, 10).then(res => {
-        updatePairs(res, "uniswap-v3",137);
-    }).catch(e => { console.log(e) });
-
-    graphql.query1(graphql.UniSwap_v2, 550).then(res => {
-        updatePairs(res, "uniswap-v2",1);
-    }).catch(e => { console.log(e) });
+		graphql.query1(graphql.QuickSwap, 140).then(res => {
+			updatePairs(res, "quickswap",137);
+		}).catch(e => { console.log(e) });
+	
+		graphql.query1(graphql.SushiSwap, 40).then(res => {
+			updatePairs(res, "sushiswap",137);
+		}).catch(e => { console.log(e) });
+	
+		graphql.query2(graphql.PancakeSwap, 10).then(res => {
+			updatePairs(res, "pancakeswap",56);
+		}).catch(e => { console.log(e) });
+	
+		graphql.query3(graphql.UniSwap_v3, 10).then(res => {
+			updatePairs(res, "uniswap-v3",137);
+		}).catch(e => { console.log(e) });
+	
+		graphql.query1(graphql.UniSwap_v2, 550).then(res => {
+			updatePairs(res, "uniswap-v2",1);
+		}).catch(e => { console.log(e) });
+    } catch (err) {
+        console.log("error:" + err.message);
+    } finally {
+        if (conn != null) conn.close();
+    }
 }
 
 
