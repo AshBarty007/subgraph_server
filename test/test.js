cd08a27fb36c3bf62e58cfd02223c600a58eb427
learnforcode <2435264793@qq.com>
2022-04-19 16:59:36 +0800

    test

--- test/test.js
+++ test/test.js
@@ -1,60 +1,82 @@
-var http = require('http')
-var hostname = "0.0.0.0"
-var port = 9002
+var graphql = require('./graphql')
+var mongodb = require('mongodb')
 
-var pairs = [{"data1":1,"data2":"ok"},{"data1":2,"data2":"ok"}]
-const server = http.createServer((req, res) => {
-	let dex = "UniSwap_V3"
-	let networkID = 1;
-	var obj;
-	switch (dex){
-        case "QuickSwap":
-            obj = {
-                QuickSwap: pairs,
-                networkID:networkID
-            };
-			break;
-        case "SushiSwap":
-            obj = {
-                SushiSwap: pairs,
-                networkID:networkID
-            };
-			break;
-        case "ApeSwap":
-            obj = {
-                ApeSwap: pairs,
-                networkID:networkID
-            };
-			break;
-        case "PancakeSwap":
-            obj = {
-                PancakeSwap: pairs,
-                networkID:networkID
-            };
-			break;
-        case "UniSwap_V2":
-            obj = {
-                UniSwap_V2: pairs,
-                networkID:networkID
-            };     
-			break;
-        case "UniSwap_V3":
-            obj = {
-                UniSwap_V3: pairs,
-                networkID:networkID
-            };        
-			break;
-        default:
-            obj = {
-                unKnow:pairs,
-                networkID:networkID
-            }                                                        
+
+const dburl = "mongodb://root:" + encodeURIComponent("Mr0s8#dFdf#8s386di2ds") + "@barterswap.cluster-ck74h9ydda33.ap-southeast-1.docdb.amazonaws.com:27017/?replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false";
+let MongoClient = mongodb.MongoClient;
+
+function UpdateData() {
+    //clearPairs();
+    graphql.query1(graphql.QuickSwap, 1, 140).then(res => {
+        updatePairs(res, "quickswap",137);
+    }).catch(e => { console.log(e) });
+
+    graphql.query1(graphql.SushiSwap, 1, 40).then(res => {
+        updatePairs(res, "sushiswap",137);
+    }).catch(e => { console.log(e) });
+
+    // graphql.query2(graphql.PancakeSwap, 2, 10).then(res => {
+    //     updatePairs(res, "pancakeswap",56);
+    // }).catch(e => { console.log(e) });
+
+    // graphql.query3(graphql.UniSwap_v3, 3, 10).then(res => {
+    //     updatePairs(res, "uniswap-v3",137);
+    // }).catch(e => { console.log(e) });
+
+    // graphql.query1(graphql.UniSwap_v2, 1, 550).then(res => {
+    //     updatePairs(res, "uniswap-v2",1);
+    // }).catch(e => { console.log(e) });
+}
+
+
+
+async function updatePairs(pairs,dex,networkID) {
+    var conn = null;
+	var obj = {
+        dex:dex,
+        networkID:networkID,
+        pairs:pairs
+    };
+    try {
+        conn = await MongoClient.connect(dburl);
+        let col = conn.db("BarterSwap").collection("Pairs");
+        await col.insertOne(obj);
+    } catch (err) {
+        console.log("error:" + err.message);
+    } finally {
+        if (conn != null) conn.close();
     }
-    res.writeHead(200,{"Content-Type":"application/json"});
-    res.end(JSON.stringify(obj));
-})
+}
 
-server.listen(port, hostname, () => {
-	console.log("server is running...")
-})
+async function clearPairs() {
+    var conn = null;
+    try {
+        conn = await MongoClient.connect(dburl);
+        let test = conn.db("BarterSwap").collection("Pairs");
+        // delete
+        test.deleteMany();
+    } catch (err) {
+        console.log("error:" + err.message);
+    } finally {
+        if (conn != null) conn.close();
+    }
+}
 
+async function findPairs(dex) {
+    var conn = null;
+    try {
+        conn = await MongoClient.connect(dburl);
+        let whereStr = {dex:dex}
+        let test = conn.db("BarterSwap").collection("Pairs");
+        pairs = await test.find(whereStr).toArray();
+	    result.result.push(pairs)
+	    return result;
+    } catch (err) {
+        console.log("error:" + err.message);
+    } finally {
+        if (conn != null) conn.close();
+    }
+}
+
+UpdateData()
+findPairs("quickswap")
