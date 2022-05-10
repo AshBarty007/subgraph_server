e9c00325cb2e107884653f50614de5fc88da8244
learnforcode <2435264793@qq.com>
2022-05-10 14:23:04 +0800

    test

--- src/server.ts
+++ src/server.ts
@@ -43,6 +43,7 @@ const server = createServer((request: IncomingMessage, response: ServerResponse)
         // }
         filter = {                        
             name: str.protocol,
+            chainId: str.chainId
         }
         dbClient.findData(TableName.DetailedPools,filter).then((result)=>{
             console.log('result',filter)
