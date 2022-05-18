d6768f5ab78b28a8fd74e3a92f8eaf3dfbc4a9ba
learnforcode <2435264793@qq.com>
2022-05-18 17:50:54 +0800

    test

--- src/providers/utils/params.ts
+++ src/providers/utils/params.ts
@@ -4,13 +4,17 @@ const request = require('request');
 export async function ethPrice():Promise<number>{
     return  new Promise(function (resolve, reject) {
         request(ETH_PRICE_API, { json: true }, (err: any, res: any, body: any) => {
-            let price
-            try{
-                price = body.data.price
-            }catch(err){
-                console.log(err)
+            if (err != null){
+                console.log("fail to get eth_price,error:",err)
+            }else{
+                let price
+                try{
+                    price = body.data.price
+                }catch(err){
+                    console.log(err)
+                }
+                resolve(price)
             }
-            resolve(price)
         })
     })
 }
