6da5b761490fac0d58bbb69ef5447285117ca21a
learnforcode <2435264793@qq.com>
2022-05-19 17:33:54 +0800

    test

--- src/providers/onchain-provider/onchian-collection.ts
+++ src/providers/onchain-provider/onchian-collection.ts
@@ -28,12 +28,11 @@ export async function onchainPools(dexName: swapName, chainId: ChainId) {
             onchainQuery = queryUniSwapV3OnChain
             break;
     }
-    console.log('dexName', dexName)
     await DB.findData(TableName.SimplePools, { name: dexName }).then((poolsData) => {
         let poolsJson = JSON.parse(poolsData)
-        console.log('poolsJson', dexName, poolsJson)
-        console.log('poolsJson.result',dexName,  poolsJson[0].result)
-        console.log('poolsJson.result.pairs',dexName,  poolsJson[0].result.pairs)
+        // console.log('poolsJson', dexName, poolsJson)
+        // console.log('poolsJson.result',dexName,  poolsJson[0].result)
+        // console.log('poolsJson.result.pairs',dexName,  poolsJson[0].result.pairs)
         let len = poolsJson[0].result.pairs.length
         console.log('len',dexName,  len)
     })
