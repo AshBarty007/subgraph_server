4a93a4348144aa482b85f90111b50daaa30eec5a
learnforcode <2435264793@qq.com>
2022-05-22 17:42:23 +0800

    test

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import { createServer, IncomingMessage, ServerResponse } from 'http';
 import { BarterSwapDB, TableName } from './mongodb/client'
+import { dexName } from './providers/utils/params';
 const url = require('url')
 
 const port = 9002;
@@ -17,17 +18,24 @@ const server = createServer((request: IncomingMessage, response: ServerResponse)
         }
         console.log('filter',filter)
         dbClient.findData(TableName.SimplePools,filter).then((result:any) => {
+            result = JSON.parse(result)
             let pools = new Map();
-            console.log(result)
-            // for (let i=0;i<dex.length;i++){
-            //     try{
-            //         let data = JSON.parse(result[i])
-            //         console.log(i,data)
-            //         pools.set(dex[i],data.result.pairs)
-            //     }catch(err){
-            //         console.log("error by returning db data,",err)
-            //     }
-            // }
+            //console.log(result)
+            for (let i=0;i<dex.length;i++){
+                try{
+                    let data = JSON.parse(result[i])
+                    console.log(i,data)
+                    if (dex[i]!=dexName.uniswap_v3){
+                        console.log(dex[i],data.result.pairs.length)
+                        pools.set(dex[i],data.result.pairs)
+                    }else{
+                        console.log(dex[i],data.result.pools.length)
+                        pools.set(dex[i],data.result.pools)
+                    }
+                }catch(err){
+                    console.log("error by returning db data,",err)
+                }
+            }
             response.writeHead(200, { "Content-Type": "application/json" });
             response.end(JSON.stringify(result));
         }).catch((err) => {
