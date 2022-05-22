8cf913d1dce70b7065d845714380b46f7cdbe1c0
learnforcode <2435264793@qq.com>
2022-05-22 21:44:09 +0800

    test

--- src/server.ts
+++ src/server.ts
@@ -5,17 +5,18 @@ const url = require('url')
 
 const port = 9002;
 const dbClient = new BarterSwapDB();
-let pools = {
-    pancakeswap: null,
-    quickswap: null,
-    sushiswap: null,
-    uniswap_v2: null,
-    uniswap_v3: null,
-}
 
 const server = createServer((request: IncomingMessage, response: ServerResponse) => {
     let http_url = request.url;
     let ok = url.parse(http_url, true);
+    let pools = {
+        pancakeswap: String,
+        quickswap: String,
+        sushiswap: String,
+        uniswap_v2: String,
+        uniswap_v3: String,
+    }
+    
     if (ok.path != '/favicon.ico') {
         let str: any = JSON.stringify(ok.query);
         str = JSON.parse(str);
@@ -62,12 +63,6 @@ const server = createServer((request: IncomingMessage, response: ServerResponse)
             });
             response.writeHead(200, { "Content-Type": "text/plain" });
             response.end("url error!");
-        }).finally(()=>{
-            pools.pancakeswap = null
-            pools.sushiswap = null
-            pools.uniswap_v2 = null
-            pools.uniswap_v3 = null
-            pools.quickswap = null
         })
     }
 
