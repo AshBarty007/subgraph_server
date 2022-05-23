cee07c62fdccf525f516f723d3fee1b1e14b8a5f
learnforcode <2435264793@qq.com>
2022-05-23 11:40:51 +0800

    test

--- src/server.ts
+++ src/server.ts
@@ -18,52 +18,61 @@ const server = createServer((request: IncomingMessage, response: ServerResponse)
     }
     
     if (ok.path != '/favicon.ico') {
-        let str: any = JSON.stringify(ok.query);
-        str = JSON.parse(str);
-        let dex = str.protocol.split(',');
-        let filter = {
-            name: { "$in": dex },
-        }
-        dbClient.findData(TableName.SimplePools, filter).then((ret: any) => {
-            let result = JSON.parse(ret)
-            for (let i = 0; i < dex.length; i++) {
-                try {
-                    switch (dex[i]) {
-                        case dexName.uniswap_v3:
-                            console.log(i,result[i].name)
-                            pools.uniswap_v3 = result[i].result.pools;
-                            break;
-                        case dexName.uniswap_v2:
-                            console.log(i,result[i].name)
-                            pools.uniswap_v2 = result[i].result.pairs;
-                            break;
-                        case dexName.sushiswap:
-                            console.log(i,result[i].name)
-                            pools.sushiswap = result[i].result.pairs;
-                            break;
-                        case dexName.quickswap:
-                            console.log(i,result[i].name)
-                            pools.quickswap = result[i].result.pairs;
-                            break;
-                        case dexName.pancakeswap:
-                            console.log(i,result[i].name)
-                            pools.pancakeswap = result[i].result.pairs;
-                            break;
-                    }
-                } catch (err) {
-                    console.log("error by returning db data,", err)
-                }
+        try{
+            let str: any = JSON.stringify(ok.query);
+            str = JSON.parse(str);
+            if (str.protocol == null){
+                response.writeHead(200, { "Content-Type": "text/plain" });
+                response.end("url error!");
             }
-            response.writeHead(200, { "Content-Type": "application/json" });
-            response.end(JSON.stringify(pools));
-        }).catch((err) => {
-            console.log(err)
-            response.on('error', (err) => {
-                console.error(err);
-            });
+            let dex = str.protocol.split(',');
+            let filter = {
+                name: { "$in": dex },
+            }
+            dbClient.findData(TableName.SimplePools, filter).then((ret: any) => {
+                let result = JSON.parse(ret)
+                for (let i = 0; i < dex.length; i++) {
+                    try {
+                        switch (dex[i]) {
+                            case dexName.uniswap_v3:
+                                console.log(i,result[i].name)
+                                pools.uniswap_v3 = result[i].result.pools;
+                                break;
+                            case dexName.uniswap_v2:
+                                console.log(i,result[i].name)
+                                pools.uniswap_v2 = result[i].result.pairs;
+                                break;
+                            case dexName.sushiswap:
+                                console.log(i,result[i].name)
+                                pools.sushiswap = result[i].result.pairs;
+                                break;
+                            case dexName.quickswap:
+                                console.log(i,result[i].name)
+                                pools.quickswap = result[i].result.pairs;
+                                break;
+                            case dexName.pancakeswap:
+                                console.log(i,result[i].name)
+                                pools.pancakeswap = result[i].result.pairs;
+                                break;
+                        }
+                    } catch (err) {
+                        console.log("error by returning db data,", err)
+                    }
+                }
+                response.writeHead(200, { "Content-Type": "application/json" });
+                response.end(JSON.stringify(pools));
+            }).catch((err) => {
+                console.log(err)
+                response.on('error', (err) => {
+                    console.error(err);
+                });
+                response.writeHead(200, { "Content-Type": "text/plain" });
+                response.end("url error!");
+            })
+        }catch(err){
             response.writeHead(200, { "Content-Type": "text/plain" });
             response.end("url error!");
-        })
+        }
     }
 
 });
