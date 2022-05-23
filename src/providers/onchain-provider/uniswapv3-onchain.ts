a93b039da95d2ddbcbd1f22829a3580424dfda3e
learnforcode <2435264793@qq.com>
2022-05-23 15:03:20 +0800

    test

--- src/providers/onchain-provider/uniswapv3-onchain.ts
+++ src/providers/onchain-provider/uniswapv3-onchain.ts
@@ -63,6 +63,7 @@ async function getPoolState(poolContract: ethers.Contract) {
 }
 
 export async function queryUniSwapV3OnChain(chainId: ChainId, poolAddress: string, token0Address: string, token1Address: string,price:number) {
+  console.log("poolAddress:",poolAddress)
   const provider = new ethers.providers.JsonRpcProvider(CHAIN_RPC[chainId])
   const poolContract = new ethers.Contract(poolAddress, IUniswapV3PoolABI, provider)
   const [immutables, state] = await Promise.all([getPoolImmutables(poolContract), getPoolState(poolContract)])
@@ -78,7 +79,7 @@ export async function queryUniSwapV3OnChain(chainId: ChainId, poolAddress: strin
       id: token1Address,
     }
   }
-console.log(result)
+
   return JSON.stringify(result)
 }
 
