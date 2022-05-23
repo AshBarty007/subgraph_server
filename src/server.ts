8a34ef2f6a8c7a10b4386eb004cd48c138a42da7
learnforcode <2435264793@qq.com>
2022-05-23 11:45:03 +0800

    test

--- src/server.ts
+++ src/server.ts
@@ -16,62 +16,64 @@ const server = createServer((request: IncomingMessage, response: ServerResponse)
         uniswap_v2: String,
         uniswap_v3: String,
     }
-    
+
     if (ok.path != '/favicon.ico') {
-        try{
+        try {
             let str: any = JSON.stringify(ok.query);
             str = JSON.parse(str);
-            if (str.protocol == null){
+            if (str.protocol == null) {
                 response.writeHead(200, { "Content-Type": "text/plain" });
                 response.end("url error!");
-            }
-            let dex = str.protocol.split(',');
-            let filter = {
-                name: { "$in": dex },
-            }
-            dbClient.findData(TableName.SimplePools, filter).then((ret: any) => {
-                let result = JSON.parse(ret)
-                for (let i = 0; i < dex.length; i++) {
-                    try {
-                        switch (dex[i]) {
-                            case dexName.uniswap_v3:
-                                console.log(i,result[i].name)
-                                pools.uniswap_v3 = result[i].result.pools;
-                                break;
-                            case dexName.uniswap_v2:
-                                console.log(i,result[i].name)
-                                pools.uniswap_v2 = result[i].result.pairs;
-                                break;
-                            case dexName.sushiswap:
-                                console.log(i,result[i].name)
-                                pools.sushiswap = result[i].result.pairs;
-                                break;
-                            case dexName.quickswap:
-                                console.log(i,result[i].name)
-                                pools.quickswap = result[i].result.pairs;
-                                break;
-                            case dexName.pancakeswap:
-                                console.log(i,result[i].name)
-                                pools.pancakeswap = result[i].result.pairs;
-                                break;
-                        }
-                    } catch (err) {
-                        console.log("error by returning db data,", err)
-                    }
+            } else {
+                let dex = str.protocol.split(',');
+                let filter = {
+                    name: { "$in": dex },
                 }
-                response.writeHead(200, { "Content-Type": "application/json" });
-                response.end(JSON.stringify(pools));
-            }).catch((err) => {
-                console.log(err)
-                response.on('error', (err) => {
-                    console.error(err);
-                });
-                response.writeHead(200, { "Content-Type": "text/plain" });
-                response.end("url error!");
-            })
-        }catch(err){
+                dbClient.findData(TableName.SimplePools, filter).then((ret: any) => {
+                    let result = JSON.parse(ret)
+                    for (let i = 0; i < dex.length; i++) {
+                        try {
+                            switch (dex[i]) {
+                                case dexName.uniswap_v3:
+                                    console.log(i, result[i].name)
+                                    pools.uniswap_v3 = result[i].result.pools;
+                                    break;
+                                case dexName.uniswap_v2:
+                                    console.log(i, result[i].name)
+                                    pools.uniswap_v2 = result[i].result.pairs;
+                                    break;
+                                case dexName.sushiswap:
+                                    console.log(i, result[i].name)
+                                    pools.sushiswap = result[i].result.pairs;
+                                    break;
+                                case dexName.quickswap:
+                                    console.log(i, result[i].name)
+                                    pools.quickswap = result[i].result.pairs;
+                                    break;
+                                case dexName.pancakeswap:
+                                    console.log(i, result[i].name)
+                                    pools.pancakeswap = result[i].result.pairs;
+                                    break;
+                            }
+                        } catch (err) {
+                            console.log("error by returning db data,", err)
+                        }
+                    }
+                    response.writeHead(200, { "Content-Type": "application/json" });
+                    response.end(JSON.stringify(pools));
+                }).catch((err) => {
+                    console.log(err)
+                    response.on('error', (err) => {
+                        console.error(err);
+                    });
+                    response.writeHead(200, { "Content-Type": "text/plain" });
+                    response.end("url error!");
+                })
+            }
+        } catch (err) {
+            console.log("fail to run server", err)
             response.writeHead(200, { "Content-Type": "text/plain" });
-            response.end("url error!");
+            response.end("fail to run server");
         }
     }
 
