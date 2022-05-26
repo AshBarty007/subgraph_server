5219c785f1da5e339fc051fd1d0ff938534cfdf9
learnforcode <2435264793@qq.com>
2022-05-26 15:54:17 +0800

    test

--- test/testDB.ts
+++ test/testDB.ts
@@ -4,7 +4,7 @@ async function test(){
    let DB = new BarterSwapDB()
    await DB.insertData("TestTable",{name:"first",value:"1"}).catch((err)=>{console.log("insert err",err)})
    await DB.findData("TestTable",{name:"first"}).then((data)=>{console.log("1.data",data)})
-   await DB.updateData("TestTable",{name:"first"},{append:"2"}).catch((err)=>{console.log("update err",err)})
+   await DB.updateData("TestTable",{name:"first"},{value:"2"}).catch((err)=>{console.log("update err",err)})
    await DB.findData("TestTable",{name:"first"}).then((data)=>{console.log("2.data",data)})
    await DB.deleteData("TestTable",{name:"first"}).catch((err)=>{console.log("delete err",err)})
    await DB.findData("TestTable",{name:"first"}).then((data)=>{console.log("3.data",data)})
