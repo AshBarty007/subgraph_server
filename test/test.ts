e458563ebb128f19b1d9c90aee273d67d40d81af
learnforcode <2435264793@qq.com>
2022-05-26 18:39:24 +0800

    test

--- test/test.ts
+++ test/test.ts
@@ -1,30 +1,20 @@
 
-async function test() {
+async function test(n:Number) {
     return new Promise(function (resolve, reject) {
         setTimeout(function () {
             console.log("First");
-            resolve(1);
+            resolve(n);
         }, 1000);
     })
-    // .then(()=> {
-    //     return new Promise(function (resolve, reject) {
-    //         setTimeout(function () {
-    //             console.log("Second");
-    //             resolve(2);
-    //         }, 4000);
-    //     });
-    // })
-    // .then(()=> {
-    //     setTimeout(function () {
-    //         console.log("Third");
-    //     }, 3000);
-    // });
 }
 
 async function start() {
-    for (let i = 0; i < 5; i++) {
-        console.log("start")
-        await test()
+    let fns = []
+    for (let i = 0; i < 10; i++) {
+        fns[i] = test(i)
     }
+    await Promise.race(fns)
+    let result = await Promise.all(fns);
+    console.log(result)
 }
 start()
