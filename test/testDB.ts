a79f0eb0514b7a27e383d7fd11833649b69093f8
learnforcode <2435264793@qq.com>
2022-05-26 16:10:01 +0800

    test

--- test/testDB.ts
+++ test/testDB.ts
@@ -6,7 +6,7 @@ async function test(){
    await DB.findData("TestTable",{name:"first"}).then((data)=>{console.log("1.data",data)})
    await DB.updateData("TestTable",{name:"first"},{$set:{name:"second"}}).catch((err)=>{console.log("update err",err)})
    await DB.findData("TestTable",{name:"second"}).then((data)=>{console.log("2.data",data)})
-   await DB.deleteData("TestTable",{name:"second"}).catch((err)=>{console.log("delete err",err)})
+   await DB.deleteData("TestTable",{name:"second"},true).catch((err)=>{console.log("delete err",err)})
    await DB.findData("TestTable",{name:"second"}).then((data)=>{console.log("3.data",data)})
    return
 }
