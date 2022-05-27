95df3d096d640a22cb99cc37de5d622b2b8a4eb6
learnforcode <2435264793@qq.com>
2022-05-27 16:47:08 +0800

    test

--- src/updateOnChainPools.ts
+++ src/updateOnChainPools.ts
@@ -4,7 +4,7 @@ import { dexName } from './providers/utils/params'
 const schedule = require('node-schedule');
 
 const scheduleTask = () => {
-    schedule.scheduleJob('59 */45 * * * *', () => {
+    schedule.scheduleJob('59 */6 * * * *', () => {
         try{
             onchainPools(dexName.pancakeswap,ChainId.BSC)
             onchainPools(dexName.quickswap,ChainId.POLYGON)
@@ -18,18 +18,4 @@ const scheduleTask = () => {
     });
 }
 
-let start = new Date().toLocaleString()
-//scheduleTask();
-try{
-    onchainPools(dexName.pancakeswap,ChainId.BSC)
-    onchainPools(dexName.quickswap,ChainId.POLYGON)
-    onchainPools(dexName.sushiswap,ChainId.POLYGON)
-    onchainPools(dexName.uniswap_v2,ChainId.MAINNET)
-    onchainPools(dexName.uniswap_v3,ChainId.POLYGON)
-}catch(err){
-    console.log("fail to update OnChainPools ,error:",err)
-}finally{
-    let end = new Date().toLocaleString()
-    console.log(start)
-    console.log(end)
-}
+scheduleTask();
