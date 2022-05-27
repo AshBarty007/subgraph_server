8f8c157e8319130f829a5b7b0b6dc79c61b4430e
learnforcode <2435264793@qq.com>
2022-05-27 16:37:24 +0800

    test

--- test/test.ts
+++ test/test.ts
@@ -4,12 +4,12 @@ async function Test() {
     await retry(
         async (bail, number) => {
             console.log("retry number", number)
-            bail(new Error('Unauthorized'));
-            //throw("test error")
+            //bail(new Error('Unauthorized'));
+            throw("test error")
         },
         { retries: 2, maxTimeout: 2000, onRetry: (err, retry) => { console.log("fail to get eth price, error message:", err, ",retry times:", retry) } }
-    ).catch((err) => {
-        console.log("throw error", err)
+    ).catch(() => {
+        //Preventing abnormal exits
     })
 
     console.log("print me")
