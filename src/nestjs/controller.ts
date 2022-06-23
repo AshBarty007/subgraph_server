f6289d2653817b9289ce6f3f967aeeda6c22b430
learnforcode <2435264793@qq.com>
2022-06-23 11:41:06 +0800

    test

--- src/nestjs/controller.ts
+++ src/nestjs/controller.ts
@@ -1,21 +1,12 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './service';
 
-@Controller()
+@Controller('get-pools')
 export class AppController {
   constructor(private readonly appService: AppService) {}
-  
-  @Get('test')
-  test(): string {
-    console.log('protocol :');
-    console.log('return :', this.appService.test());
-    return this.appService.test();
-  }
 
-  @Get('get-pools')
-  findAll(@Query('protocol') protocol: any): Promise<void> {
-    console.log('protocol :', protocol);
-    console.log('return :', this.appService.getPools(protocol));
+  @Get()
+  findAll(@Query('protocol') protocol:any): Promise<void> {
     return this.appService.getPools(protocol);
   }
 }
